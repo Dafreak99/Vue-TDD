0bdb716c4b0f111b8d89c5f6ea07e140f29141e2
hai.tran <hai.tran@year13.com.au>
2022-03-25 16:57:04 +0700

    update content

--- tests/unit/SubmitButton.spec.js
+++ tests/unit/SubmitButton.spec.js
@@ -1,36 +1,27 @@
 import { mount } from "@vue/test-utils";
 import SubmitButton from "@/components/SubmitButton";
 
-// const factory = (propsData) => {
-//   return mount(SubmitButton, {
-//     propsData: {
-//       msg,
-//       ...propsData,
-//     },
-//   });
-// };
+const factory = (propsData) => {
+  return mount(SubmitButton, {
+    propsData: {
+      msg,
+      ...propsData,
+    },
+  });
+};
 
 const msg = "submit";
 
 describe("SubmitButton.vue", () => {
   it("displays a non authorized message", () => {
-    const wrapper = mount(SubmitButton, {
-      propsData: {
-        msg,
-      },
-    });
+    const wrapper = factory({ msg });
 
     expect(wrapper.find("span").text()).toBe("Not Authorized");
     expect(wrapper.find("button").text()).toBe(msg);
   });
 
   it("displays a privileges message", () => {
-    const wrapper = mount(SubmitButton, {
-      propsData: {
-        msg,
-        isAdmin: true,
-      },
-    });
+    const wrapper = factory({ msg, isAdmin: true });
 
     expect(wrapper.find("span").text()).toBe("Admin Privileges");
     expect(wrapper.find("button").text()).toBe(msg);
